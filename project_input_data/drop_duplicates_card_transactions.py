41e170d5dba2b1c8f88df54f7e143ae14d82a1df
Ram Pravesh <rpravesh50497@gmail.com>
2023-11-07 22:31:21 +0530

    adding py script to remove duplicates on column card_id & transaction_dt

---
+++ project_input_data/drop_duplicates_card_transactions.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+my_conf = SparkConf()
+my_conf.set("spark.app.name", "my first application")
+my_conf.set("spark.master","local[*]")
+spark = SparkSession.builder.config(conf=my_conf).getOrCreate()
+
+Df = spark.read.csv("C:/Users/rpravesh/Desktop/card_transactions.csv")
+print(Df.distinct().count())
+Df.show()
+
+df1 = Df.select('_c0','_c5')
+print(df1.distinct().count())
+
+df2=Df.dropDuplicates(["_c0","_c5"])
+print(df2.distinct().count())
+df2.show()
+
+df2.coalesce(1).write\
+    .format("csv")\
+    .mode('overwrite')\
+    .save("C:/Users/rpravesh/Desktop/credit-card-fraud-detection/project_input_data")
+
+spark.stop()
