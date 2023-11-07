a2cfce78f24771b823a7db6a4a600a273aadaca5
Ram Pravesh <147907274+ram-pravesh@users.noreply.github.com>
2023-11-07 22:34:34 +0530

    Updating app name drop_duplicates_card_transactions.py

--- project_input_data/drop_duplicates_card_transactions.py
+++ project_input_data/drop_duplicates_card_transactions.py
@@ -2,7 +2,7 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 my_conf = SparkConf()
-my_conf.set("spark.app.name", "my first application")
+my_conf.set("spark.app.name", "card_transactions remove duplicates")
 my_conf.set("spark.master","local[*]")
 spark = SparkSession.builder.config(conf=my_conf).getOrCreate()
 
